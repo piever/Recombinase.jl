a6227ecbc027c0edf7d74ff13ebd44c5452558ba
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-16 17:54:08 +0000

    update examples again

--- test/examples.jl
+++ test/examples.jl
@@ -1,19 +1,15 @@
 using Statistics, StatsBase
-using StructArrays
-using GroupSummaries
-using GroupSummaries: summaries
-using Test
-using IndexedTables
+using GroupSummaries: summaries, density, frequency, hazard, localregression
 using RDatasets
 
 t = RDatasets.dataset("mlmRev","Hsb82")
 
 summaries(t.School, t.MAch, t.SSS)
 
-summaries(GroupSummaries.density, t.School, t.MAch, summarize = (mean, sem))
+summaries(density, t.School, t.MAch, summarize = (mean, sem))
 
-summaries(GroupSummaries.frequency, t.School, rand(1:100, length(t.School)), summarize = (mean, sem))
+summaries(frequency, t.School, rand(1:100, length(t.School)), summarize = (mean, sem))
 
-summaries(GroupSummaries.hazard, t.School, t.MAch, summarize = (mean, sem))
+summaries(hazard, t.School, t.MAch, summarize = (mean, sem))
 
-summaries(GroupSummaries.localregression, t.School, t.MAch, t.SSS)
+summaries(localregression, t.School, t.MAch, t.SSS)
