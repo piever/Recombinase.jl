b02ec2b0f2f39a39558b0b7caf5d44ffb5e2f10a
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-20 16:31:00 +0000

    more examples

--- examples/tutorial.jl
+++ examples/tutorial.jl
@@ -1,7 +1,7 @@
 using Statistics, StatsBase
 using GroupSummaries: Group, series2D, datafolder
-using GroupSummaries: compute_error, density, frequency, hazard, localregression
-using JuliaDB, Plots
+using GroupSummaries: compute_error, density, frequency, hazard, localregression, cumulative, expectedvalue
+using JuliaDB, StatsPlots
 
 data = loadtable(joinpath(datafolder, "school.csv"))
 
@@ -28,3 +28,35 @@ args, kwargs = series2D(
     summarize = mean
     )
 scatter(args...; kwargs...)
+
+args, kwargs = series2D(
+    cumulative,
+    data,
+    Group((:Sx, :Sector)),
+    across = :School,
+    select = :MAch,
+    ribbon = true
+   )
+plot(args...; kwargs..., legend = :bottomright)
+    
+args, kwargs = series2D(
+    density(bandwidth = 1),
+    data,
+    Group(color=:Sx, linestyle=:Sector),
+    across = :School,
+    select = :MAch,
+    ribbon = true
+   )
+plot(args...; kwargs...)
+    
+args, kwargs = series2D(
+    expectedvalue,
+    data,
+    Group(color = :Minrty),
+    select = (:Sx, :MAch),
+    summarize = (mean, sem),
+    across = 1:length(data)
+)
+groupedbar(args...; kwargs...)
+
+##
