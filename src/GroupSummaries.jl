43efabf2c4ae0999d8214bcce8e33aa1ea1520b9
DarioSarra <[dariosarra@gmail.com]>
2019-03-20 18:23:08 +0000

    implemented gui

--- src/GroupSummaries.jl
+++ src/GroupSummaries.jl
@@ -8,6 +8,9 @@ using KernelDensity: pdf, kde
 using StructArrays: StructVector, finduniquesorted, fieldarrays
 using IndexedTables: collect_columns, collect_columns_flattened, rows, columns, sortpermby, IndexedTable
 import ColorBrewer
+import Widgets
+using Widgets: Widget, dropdown, toggle, button
+using OrderedCollections: OrderedDict
 
 datafolder = joinpath(@__DIR__, "..", "data")
 
@@ -15,5 +18,6 @@ include("analysisfunctions.jl")
 include("compute_error.jl")
 include("styles.jl")
 include("groupsummaries.jl")
+include("gui.jl")
 
 end # module
---
+++ src/gui.jl
@@ -0,0 +1,64 @@
+const analysis_options = OrderedDict(
+    "" => nothing,
+    "Cumulative" => GroupSummaries.cumulative,
+    "Density" => GroupSummaries.density,
+    "Frequency" => GroupSummaries.frequency,
+    "Hazard" => GroupSummaries.hazard,
+    "Local Regression" => GroupSummaries.localregression,
+    "Expected Value" => GroupSummaries.expectedvalue
+)
+
+"""
+`gui(data, plotters)`
+
+Create a gui around `data::IndexedTable` given a list of plotting
+functions plotters.
+
+## Examples
+
+```julia
+using StatsPlots, GroupSummaries, JuliaDB
+school = loadtable(joinpath(GroupSummaries.datafolder, "school.csv"))
+plotters = [plot, scatter, groupedbar]
+gui(school, plotters)
+``` 
+"""
+function gui(data, plotters)
+    (data isa Observables.AbstractObservable) || (data = Observable{Any}(data))
+    ns = Observables.@map collect(colnames(&data))
+    maybens = Observables.@map vcat(Symbol(), &ns)
+    xaxis = dropdown(ns,label = "X")
+    yaxis = dropdown(maybens,label = "Y")
+    an_opt = dropdown(analysis_options, label = "Analysis")
+    across = dropdown(ns, label="Across")
+    styles = collect(keys(style_dict))
+    sort!(styles)
+    splinters = [dropdown(maybens, label = string(style)) for style in styles]
+    plotter = dropdown(plotters, label = "Plotter")
+    ribbon = toggle("Ribbon", value = true)
+    btn = button("Plot")
+    output = Observable{Any}(plot())
+    Observables.@map! output begin
+        &btn
+        select = yaxis[] == Symbol() ? xaxis[] : (xaxis[], yaxis[])
+        grps = Dict(key => val[] for (key, val) in zip(styles, splinters) if val[] != Symbol())
+        args, kwargs = series2D(an_opt[], &data, Group(; grps...);
+            select = select, across = across[], ribbon = ribbon[])
+        plotter[](args...; kwargs...)
+    end
+    ui = Widget(
+        OrderedDict(
+            :xaxis => xaxis,
+            :yaxis => yaxis,
+            :analysis => an_opt,
+            :across => across,
+            :plotter => plotter,
+            :plot_button => btn,
+            :ribbon => ribbon,
+            :splinters => splinters,
+        ),
+        output = output
+    )
+    Widgets.@layout! ui Widgets.div(Widgets.div(:xaxis, :yaxis, :analysis, :across, :plotter), :ribbon, :plot_button,
+        Widgets.div(Widgets.div(:splinters...), output))
+end
