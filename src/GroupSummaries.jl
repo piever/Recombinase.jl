33854704dff86d3a984887584255b986b644796d
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-04-05 14:21:58 +0100

    Support timeseries (#8)
    * add support for timeseries with OnlineStats
    
    * remove deps
    
    * tests

--- src/GroupSummaries.jl
+++ src/GroupSummaries.jl
@@ -5,7 +5,7 @@ using Statistics
 using StatsBase: countmap, ecdf, sem
 using Loess: loess, predict
 using KernelDensity: pdf, kde
-using StructArrays: StructVector, finduniquesorted, fieldarrays
+using StructArrays: StructVector, StructArray, finduniquesorted, fieldarrays
 using IndexedTables: collect_columns, collect_columns_flattened, rows, columns, sortpermby, IndexedTable, colnames
 import ColorBrewer
 import Widgets, Observables
@@ -15,6 +15,7 @@ using OrderedCollections: OrderedDict
 datafolder = joinpath(@__DIR__, "..", "data")
 
 include("analysisfunctions.jl")
+include("timeseries.jl")
 include("compute_error.jl")
 include("styles.jl")
 include("groupsummaries.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using GroupSummaries
 using GroupSummaries: compute_error
 using Test
 using IndexedTables
+using ShiftedArrays, OnlineStatsBase
 
 @testset "discrete" begin
     x = [1, 2, 3, 1, 2, 3]
@@ -24,6 +25,17 @@ using IndexedTables
         summarize = mean
     )
     @test res.first == [1, 2, 3]
-    @test columns(res.second, 1) ≈ [1, 1, 1]
+    @test columns(res.second, 1) ≈ [1, 1, 1]./3
 end
 
+@testset "timeseries" begin
+    v1 = rand(1000) # day 1
+    v2 = rand(50) # day 2
+    traces = [v1, v1, v1, v2, v2, v2]
+    ts = [10, 501, 733, 1, 20, 30]
+    stats = (mean = Mean, variance = Variance)
+    s = GroupSummaries.fitvec(stats, (lead(trace, t) for (trace, t) in zip(traces, ts)), -5:5);
+    @test axes(s) == (-5:5,)
+    @test s[-3].nobs == 4
+    @test s[-3].value isa NamedTuple{(:mean, :variance)}
+end
