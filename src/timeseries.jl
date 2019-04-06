b6596de62ab4f53495a813c130aa806c8c6a7182
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-04-06 18:33:08 +0100

    ignore out of bound values

--- src/timeseries.jl
+++ src/timeseries.jl
@@ -1,19 +1,23 @@
 using OnlineStatsBase: fit!, FTSeries, nobs, value
 using OffsetArrays: OffsetArray
 
-function initstats(series, ranges; dropmissing = true, filter = isfinite, transform = identity)
-    filter_func(x) = !(dropmissing && ismissing(x)) && filter(x)
+isfinitevalue(::Missing) = false
+isfinitevalue(x::Number) = isfinite(x)
+
+function initstats(series, ranges; filter = isfinitevalue, transform = identity)
     series = to_tuple(series)
     ranges = to_tuple(ranges)
     itr = Iterators.product(ranges...)
     vec = [FTSeries((stat() for stat in series)...;
-            filter = filter_func, transform = transform) for _ in itr]
+            filter = filter, transform = identity) for _ in itr]
     return OffsetArray(vec, ranges)
 end
 
 function fitvec!(m, val)
-    @inbounds for i in eachindex(m)
-        fit!(m[i], val[i])
+    for i in eachindex(m)
+        if checkbounds(Bool, val, i)
+            @inbounds fit!(m[i], val[i])
+        end
     end
     return m
 end
