f94b6d2f30824e57d984753fa34c5d52cf61c7ef
DarioSarra <dariosarra@gmail.com>
2019-12-06 11:06:48 +0000

    both dict adjusted

--- src/styles.jl
+++ src/styles.jl
@@ -1,7 +1,7 @@
 const default_style_dict = Dict(
     :color => ColorBrewer.palette("Set1", 8),
     :markercolor => ColorBrewer.palette("Set1", 8),
-    :markershape => [:diamond, :circle, :triangle, :star5],
+    :markershape => [:circle, :diamond, :utriangle, :star5, :cross],
     :linestyle => [:solid, :dash, :dot, :dashdot],
     :linewidth => [1,4,2,3],
     :markersize => [3,9,5,7]
